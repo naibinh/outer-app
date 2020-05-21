38454f4350b7dbb2b4ad0cfa6114151cf354a769
naibinh <binh.nguyen@gooddata.com>
2020-05-21 13:32:15 +0700

    commit20: re-layout Logs on KD

--- src/components/KD/App.js
+++ src/components/KD/App.js
@@ -41,12 +41,12 @@ function App() {
                             <Commands logCommand={logCommand} />
                         </Paper>
                     </Grid>
-                    <Grid item xs={9}>
+                    <Grid item xs={12}>
                         <Paper className={classes.paper}>
                             <EmbeddedGdc appName="kd" id={EMBEDDED_KD} />
                         </Paper>
                     </Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs={12}>
                         <Paper className={classes.logs}>
                             <ExpansionLogs logs={logs} clearLogs={clearLogs} />
                         </Paper>
